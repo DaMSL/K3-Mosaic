e2b6e357054f45889e40cb68ce342ee85f5e9529
P.C. Shyamshankar <pcshyamshankar@jhu.edu>
2011-12-29 16:05:36 -0500

    Add stringification for types.

--- K3.ml
+++ K3.ml
@@ -17,3 +17,30 @@ type type_t
     | TTuple        of type_t list
     | TCollection   of collection_type_t * type_t
     | TFunction     of type_t * type_t
+
+(* Utilities *)
+
+let string_of_collection_type t_c = match t_c with
+    | TSet  -> "TSet"
+    | TBag  -> "TBag"
+    | TList -> "TList"
+
+let rec string_of_type t = match t with
+    | TUnknown  -> "TUnknown"
+    | TUnit     -> "TUnit"
+    | TBool     -> "TBool"
+    | TInt      -> "TInt"
+    | TFloat    -> "TFloat"
+    | TString   -> "TString"
+
+    | TTuple(t_l)
+        -> "TTuple("^(String.concat ", " (List.map string_of_type t_l))^")"
+
+    | TCollection(t_c, t_e)
+        -> "TCollection("
+            ^string_of_collection_type(t_c)^", "
+            ^string_of_type(t_e)
+        ^")"
+
+    | TFunction(t_a, t_r)
+        -> "TFunction("^string_of_type t_a ^", "^ string_of_type t_r ^")"
--- K3.mli
+++ K3.mli
@@ -17,3 +17,8 @@ type type_t
     | TTuple        of type_t list
     | TCollection   of collection_type_t * type_t
     | TFunction     of type_t * type_t
+
+(* Utilities *)
+
+val string_of_collection_type: collection_type_t -> string
+val string_of_type: type_t -> string
