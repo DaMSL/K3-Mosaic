1c1d5ea4d64f7ea888720ad8659ada036f472c98
P.C. Shyamshankar <pcshyamshankar@jhu.edu>
2012-04-01 16:40:59 -0400

    Refactor typechecking functions to reduce parentheses.

--- K3Typechecker.ml
+++ K3Typechecker.ml
@@ -7,28 +7,39 @@ exception TypeError
 
 let type_of expr = let ((id, t), tag), children = decompose_tree expr in t
 
-let deduce_constant_type c = match c with
-    | CBool(_) -> TBool
-    | CInt(_) -> TInt
-    | CFloat(_) -> TFloat
-    | CString(_) -> TString
-    | CNothing -> TMaybe(TUnknown)
+let deduce_constant_type c = let constant_type = match c with
+        | CBool(_) -> TBool
+        | CInt(_) -> TInt
+        | CFloat(_) -> TFloat
+        | CString(_) -> TString
+        | CNothing -> TMaybe(TUnknown)
+    in RefT(BaseT(constant_type))
 
 let rec deduce_type env expr =
     let (meta, tag), untyped_children = decompose_tree expr in
+
+    (* Determine if we need to augment the type environment. *)
     let new_env = match tag with
             | Lambda(AVar(i, t)) -> (i, t) :: env
             | Lambda(ATuple(xs)) -> xs @ env
             | _ -> env
         in
-    let children = List.map (deduce_type new_env) untyped_children in
-    match tag with
-        | Const(c) -> Leaf((meta, RefT(BaseT(deduce_constant_type c))), tag)
-        | Var(id, t) -> Leaf((meta, t), tag)
-        | Tuple -> let child_types = List.map (
-                fun e -> match type_of e with 
-                    | RefT(BaseT(bt)) -> bt
-                    | _ -> raise TypeError
-                ) children in
-            Node((meta, RefT(BaseT(TTuple(child_types)))), tag, children)
-        | _ -> Node((meta, RefT(BaseT(TUnknown))), tag, children)
+
+    (* Deduce the types of all children nodes. *)
+    let typed_children = List.map (deduce_type new_env) untyped_children in
+
+    (* Define some helpers. *)
+    let attach_type t = recompose_tree (((meta, t), tag), typed_children) in
+
+    (* Deduce the type of the current node. *)
+    let current_type = match tag with
+            | Const(c) -> deduce_constant_type c
+            | Var(id, t) -> t
+            | Tuple -> let child_types = List.map (
+                    fun e -> match type_of e with
+                        | RefT(BaseT(bt)) -> bt
+                        | _ -> raise TypeError
+                    ) typed_children in
+                RefT(BaseT(TTuple(child_types)))
+            | _ -> RefT(BaseT(TUnknown))
+        in attach_type current_type
--- K3Typechecker.mli
+++ K3Typechecker.mli
@@ -4,5 +4,5 @@ open Tree
 open K3
 
 val type_of: (int * type_t) expr_t -> type_t
-val deduce_constant_type: constant_t -> base_type_t
+val deduce_constant_type: constant_t -> type_t
 val deduce_type: (id_t * type_t) list -> int expr_t -> (int * type_t) expr_t
