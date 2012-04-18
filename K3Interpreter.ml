38efa0bbc73c3f5c334659d237daaab2c1b8f9e8
P.C. Shyamshankar <pcshyamshankar@jhu.edu>
2012-04-18 16:42:26 -0400

    Rename value types for simplicity, add ref and function value types.

--- K3Interpreter.ml
+++ K3Interpreter.ml
@@ -2,24 +2,25 @@ open Tree
 open K3
 open K3Typechecker
 
-type ocaml_K3_value_t
-    = OUnit
-    | OBool of bool
-    | OByte of int
-    | OInt of int
-    | OFloat of float
-    | OString of string
-    | OTuple of ocaml_K3_value_t list
-    | ORef of ocaml_K3_value_t
-    | OMaybe of ocaml_K3_value_t option
-    | OSet of ocaml_K3_value_t list
-    | OBag of ocaml_K3_value_t list
-    | OList of ocaml_K3_value_t list
+type value_t
+    = VUnit
+    | VBool of bool
+    | VByte of int
+    | VInt of int
+    | VFloat of float
+    | VString of string
+    | VTuple of value_t list
+    | VRef of value_t ref
+    | VMaybe of value_t option
+    | VSet of value_t list
+    | VBag of value_t list
+    | VList of value_t list
+    | VFunction of arg_t * (int * type_t) expr_t
 
 exception RuntimeError
 
 let deref = function
-    | ORef(v) -> v
+    | VRef(v) -> !v
     | v -> v
 
 let rec eval env e = let ((_, t), tag), children = decompose_tree e in
@@ -27,37 +28,43 @@ let rec eval env e = let ((_, t), tag), children = decompose_tree e in
     match tag with
         | Const(c) -> (
             match c with
-                | CInt(n) -> OInt(n)
-                | CFloat(n) -> OFloat(n)
-                | CString(s) -> OString(s)
-                | CBool(b) -> OBool(b)
-                | CNothing -> OMaybe(None)
+                | CInt(n) -> VInt(n)
+                | CFloat(n) -> VFloat(n)
+                | CString(s) -> VString(s)
+                | CBool(b) -> VBool(b)
+                | CNothing -> VMaybe(None)
             )
+
+        | Var(id) -> (try List.assoc id env with Not_found -> raise RuntimeError)
+
+        | Tuple -> VTuple(List.map (eval env) children)
+        | Just -> VMaybe(Some (eval' 0))
+
 
         | Add ->
             let a, b = deref (eval' 0), deref (eval' 1) in (
                 match a, b with
-                    | OFloat(f1), OFloat(f2) -> OFloat(f1 +. f2)
-                    | OFloat(f1), OInt(i2) -> OFloat(f1 +. float_of_int i2)
-                    | OInt(i1), OFloat(f2) -> OFloat(float_of_int i1 +. f2)
-                    | OInt(i1), OInt(i2) -> OInt(i1 + i2)
+                    | VFloat(f1), VFloat(f2) -> VFloat(f1 +. f2)
+                    | VFloat(f1), VInt(i2) -> VFloat(f1 +. float_of_int i2)
+                    | VInt(i1), VFloat(f2) -> VFloat(float_of_int i1 +. f2)
+                    | VInt(i1), VInt(i2) -> VInt(i1 + i2)
                     | _ -> raise RuntimeError
                 )
         | Mult ->
             let a, b = deref (eval' 0), deref (eval' 1) in (
                 match a, b with
-                    | OFloat(f1), OFloat(f2) -> OFloat(f1 *. f2)
-                    | OFloat(f1), OInt(i2) -> OFloat(f1 *. float_of_int i2)
-                    | OInt(i1), OFloat(f2) -> OFloat(float_of_int i1 *. f2)
-                    | OInt(i1), OInt(i2) -> OInt(i1 * i2)
+                    | VFloat(f1), VFloat(f2) -> VFloat(f1 *. f2)
+                    | VFloat(f1), VInt(i2) -> VFloat(f1 *. float_of_int i2)
+                    | VInt(i1), VFloat(f2) -> VFloat(float_of_int i1 *. f2)
+                    | VInt(i1), VInt(i2) -> VInt(i1 * i2)
                     | _ -> raise RuntimeError
                 )
         | Neg ->
             let a = deref (eval' 0) in (
                 match a with
-                    | OBool(b) -> OBool(not b)
-                    | OInt(i) -> OInt(-i)
-                    | OFloat(f) -> OFloat(-.f)
+                    | VBool(b) -> VBool(not b)
+                    | VInt(i) -> VInt(-i)
+                    | VFloat(f) -> VFloat(-.f)
                     | _ -> raise RuntimeError
             )
 
@@ -65,13 +72,13 @@ let rec eval env e = let ((_, t), tag), children = decompose_tree e in
             let a, b = deref (eval' 0), deref (eval' 1) in
             let (===) = if cmp = Eq then (=) else (<>) in (
                 match a, b with
-                    | OBool(b1), OBool(b2) -> OBool(b1 === b2)
-                    | OByte(y1), OByte(y2) -> OBool(y1 === y2)
-                    | OInt(i1), OInt(i2) -> OBool(i1 === i2)
-                    | OFloat(f1), OFloat(f2) -> OBool(f1 === f2)
-                    | OString(s1), OString(s2) -> OBool(s1 === s2)
-                    | OTuple(t1), OTuple(t2) -> OBool(List.for_all2 (===) t1 t2)
-                    | OMaybe(m1), OMaybe(m2) -> OBool(m1 === m2)
+                    | VBool(b1), VBool(b2) -> VBool(b1 === b2)
+                    | VByte(y1), VByte(y2) -> VBool(y1 === y2)
+                    | VInt(i1), VInt(i2) -> VBool(i1 === i2)
+                    | VFloat(f1), VFloat(f2) -> VBool(f1 === f2)
+                    | VString(s1), VString(s2) -> VBool(s1 === s2)
+                    | VTuple(t1), VTuple(t2) -> VBool(List.for_all2 (===) t1 t2)
+                    | VMaybe(m1), VMaybe(m2) -> VBool(m1 === m2)
                     | _ -> raise RuntimeError
             )
 
@@ -79,13 +86,13 @@ let rec eval env e = let ((_, t), tag), children = decompose_tree e in
             let a, b = deref (eval' 0), deref (eval' 1) in
             let (===) = if cmp = Lt then (<) else (<=) in (
                 match a, b with
-                    | OBool(b1), OBool(b2) -> OBool(b1 === b2)
-                    | OByte(y1), OByte(y2) -> OBool(y1 === y2)
-                    | OInt(i1), OInt(i2) -> OBool(i1 === i2)
-                    | OFloat(f1), OFloat(f2) -> OBool(f1 === f2)
-                    | OString(s1), OString(s2) -> OBool(s1 === s2)
-                    | OTuple(t1), OTuple(t2) -> OBool(List.for_all2 (===) t1 t2)
-                    | OMaybe(m1), OMaybe(m2) -> OBool(m1 === m2)
+                    | VBool(b1), VBool(b2) -> VBool(b1 === b2)
+                    | VByte(y1), VByte(y2) -> VBool(y1 === y2)
+                    | VInt(i1), VInt(i2) -> VBool(i1 === i2)
+                    | VFloat(f1), VFloat(f2) -> VBool(f1 === f2)
+                    | VString(s1), VString(s2) -> VBool(s1 === s2)
+                    | VTuple(t1), VTuple(t2) -> VBool(List.for_all2 (===) t1 t2)
+                    | VMaybe(m1), VMaybe(m2) -> VBool(m1 === m2)
                     | _ -> raise RuntimeError
             )
 
