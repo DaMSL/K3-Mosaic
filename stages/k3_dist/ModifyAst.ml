e7eaf325c576de02e83cafb54bfd9515ce9b9b49
Yotam Barnoy <yotambarnoy@gmail.com>
2012-12-19 16:15:40 -0500

    ModifyAst fixed up so now RST modifies with no type errors

--- stages/k3_dist/ModifyAst.ml
+++ stages/k3_dist/ModifyAst.ml
@@ -62,26 +62,22 @@ let ast_for_s p ast stmt (trig:P.trig_name_t) =
   let s_idx = stmt_idx_in_t p trig stmt in
   block_nth trig_ast s_idx
 
+let maps_with_existing_out_tier p stmt =
+  let maps = P.map_names_ids_of_stmt p stmt in
+  let lmap = P.lhs_map_of_stmt p stmt in
+  ("existing_out_tier", lmap) :: maps
+
 (* check for a non-map (a variable) in a stmt *)
 let get_stmt_0d_maps p stmt =
-  let lmap = P.lhs_map_of_stmt p stmt in
-  let rmaps = P.rhs_maps_of_stmt p stmt in
-  let maps = ListAsSet.uniq @: lmap::rmaps in
-  List.filter (fun m -> P.map_types_no_val_for p m = []) maps
-
+  let maps = maps_with_existing_out_tier p stmt in
+  List.filter (fun (_,m) -> P.map_types_no_val_for p m = []) maps
+  
 (* Change maps with no input dimensions to access by vid *)
 let modify_0d_map_access p ast stmt =
-  let maps = get_stmt_0d_maps p stmt in
-  let lmap = P.lhs_map_of_stmt p stmt in
-  let map_names_types2 = 
-    List.map (fun m -> (P.map_name_of p m, P.map_types_with_v_for p m)) maps in
-  (* add existing_out_tier if lmap is 0 dim *)
-  let map_names_types = if List.exists (fun m -> m = lmap) maps
-    then ("existing_out_tier", P.map_types_with_v_for p lmap)::map_names_types2
-    else map_names_types2 in
-  let modify_0d_map tree m_nm_tp =
-    let m = fst m_nm_tp in
-    let m_t = snd m_nm_tp in
+  let maps_n_id = get_stmt_0d_maps p stmt in
+  let modify_0d_map tree m_nm_id =
+    let m = fst m_nm_id in
+    let m_t = P.map_types_with_v_for p @: snd m_nm_id in
     (* note: our second pass will add vid to the map lookup *)
     (*let map_lookup = ids_to_vars @: P.map_ids_add_v ["_"] in*)
     let modify e = 
@@ -91,31 +87,101 @@ let modify_0d_map_access p ast stmt =
                mk_tuple [mk_const CUnknown] (* map_lookup *)
       else e in
     T.modify_tree_bu tree modify in
-  List.fold_left modify_0d_map ast map_names_types
+  List.fold_left modify_0d_map ast maps_n_id
 
 exception UnhandledModification of string
+
+(* modify the internals of a type. A function gets both the unwrapped type and
+ * the original type (wrapped) in case it wants to use that *)
+let modify_tuple_type fn typ = 
+  let unwrap_m = function
+    | TMutable(t, a)   -> TMutable(fn t typ, a)
+    | TImmutable(t, a) -> TImmutable(fn t typ, a)
+  in match typ with
+    | TIsolated(m)  -> TIsolated(unwrap_m m)
+    | TContained(m) -> TContained(unwrap_m m)
 
 (* add vid to all map accesses *)
 let modify_map_access p ast stmt =
   let lmap = P.lhs_map_of_stmt p stmt in
   let lmap_name = P.map_name_of p lmap in
   let lmap_types = P.map_types_with_v_for p lmap in
+  let maps_n_id = maps_with_existing_out_tier p stmt in
   let var_vid = mk_var "vid" in
-  let modify e = match U.tag_of_expr e with
+  let modify e path = match U.tag_of_expr e with
     | Insert -> let (col, elem) = U.decompose_insert e in
       set_modified @: mk_insert col @:
         begin match U.tag_of_expr elem with
-          | Tuple  -> let xs = U.decompose_tuple e in
+          | Tuple  -> let xs = U.decompose_tuple elem in
                       mk_tuple @: P.map_add_v var_vid xs
           | x      -> mk_tuple @: P.map_add_v var_vid [elem]
         end
-    | Slice -> let (col, pat) = U.decompose_slice e in
-      set_modified @: mk_slice col @:
-        begin match U.tag_of_expr pat with
-          | Tuple -> let xs = U.decompose_tuple e in
-                     mk_tuple @: P.map_add_v var_vid xs
-          | x     -> mk_tuple @: P.map_add_v var_vid [pat]
+    | Delete -> let (col, elem) = U.decompose_delete e in
+      set_modified @: mk_delete col @:
+        begin match U.tag_of_expr elem with
+          | Tuple  -> let xs = U.decompose_tuple elem in
+                      mk_tuple @: P.map_add_v var_vid xs
+          | x      -> mk_tuple @: P.map_add_v var_vid [elem]
         end
+    | Slice -> (* first check what's our parent *)
+      (* a simple modification of a slice. Useful for structural access to the
+       * map, such as when deleting *)
+      let modify_slice e = 
+        let (col, pat) = U.decompose_slice e in
+          set_modified @: mk_slice col @:
+            begin match U.tag_of_expr pat with
+              | Tuple -> let xs = U.decompose_tuple pat in
+                         mk_tuple @: P.map_add_v var_vid xs
+              | x     -> mk_tuple @: P.map_add_v var_vid [pat]
+            end
+      in 
+      (* a projected slice modification. Useful for access to the data of the
+       * map *)
+      let project_modify_slice e = 
+        let (col, pat) = U.decompose_slice e in
+        begin match U.tag_of_expr col with 
+          | Var id -> 
+            let m = List.assoc id maps_n_id in
+            let m_id_t = P.map_ids_types_for p m in
+            let m_ids = extract_arg_names m_id_t in
+            let m_id_t_v = P.map_ids_types_with_v_for p m in 
+            (* don't set modified -- looks unmodified because projected *)
+            mk_map  
+              (mk_lambda (wrap_args m_id_t_v) @:
+                mk_tuple @: ids_to_vars @: m_ids
+              ) @: modify_slice e
+          | _ -> raise (UnhandledModification ("Cannot handle non-var in slice"))
+        end in
+      let parent = list_head path in
+      begin match U.tag_of_expr parent with
+       (* if we have delete above us, we need the full structure of the map. If
+        * not, we can project onto a collection without the vid *)
+        | Delete -> modify_slice e
+        | Iterate -> let (lambda, _) = U.decompose_iterate parent in
+          let body = U.decompose_lambda lambda in
+          begin match U.tag_of_expr body with
+            | Delete -> modify_slice e
+            | _ -> project_modify_slice e
+          end
+        | _      -> project_modify_slice e
+      end
+     (* handle a case where we're iterating over a collection that's modified *)
+    | Iterate -> let (lambda, col) = U.decompose_iterate e in
+      if is_modified col then
+        let body = U.decompose_lambda lambda in
+        let vid_avar = AVar("vid", t_vid) in
+        let args = U.arg_of_lambda lambda in
+        let mod_fn typ orig_typ = match typ with
+          | TTuple(ts) -> TTuple(P.map_add_v t_vid ts)
+          | t -> TTuple(P.map_add_v t_vid [orig_typ])
+        in let new_args = begin match args with
+          | Some(ATuple(vs)) -> ATuple(P.map_add_v vid_avar vs)
+          | Some(v) -> ATuple(P.map_add_v vid_avar [v])
+          (*| Some(AVar(v, t)) -> AVar(v, modify_tuple_type mod_fn t)*)
+          | _ -> raise(UnhandledModification("lambda not found"))
+        end
+        in set_modified @: mk_iter (mk_lambda new_args body) col
+      else e
     (* handle a case when the map is applied to a lambda *)
     | Apply  -> let (l, arg) = U.decompose_apply e in
       begin match U.tag_of_expr arg with
@@ -131,12 +197,12 @@ let modify_map_access p ast stmt =
         | _ -> mk_apply l arg
       end
     | _ -> e
-  in T.modify_tree_bu ast modify
+  in T.modify_tree_bu_with_path ast modify
 
 (* return a modified version of the original ast for s *)
 let modify_ast_for_s p ast stmt trig = 
   let ast = ast_for_s p ast stmt trig in
-  let ast = modify_0d_map_access p ast stmt in
   let ast = modify_map_access p ast stmt in
+  let ast = modify_0d_map_access p ast stmt in
   clean_ast_meta ast (* remove the meta info we used *)
 
