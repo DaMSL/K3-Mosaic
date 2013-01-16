f51c0a565719d1e09820dc9857d11db612b84943
Yotam Barnoy <yotambarnoy@gmail.com>
2013-01-16 16:18:45 -0500

    Added peers to globals
    Currently it's only a static global variable. We need to think how to
    inject the dynamic peers (ie. the contents of peers) into the
    interpreter.

--- src/stages/k3/K3Global.ml
+++ src/stages/k3/K3Global.ml
@@ -8,11 +8,29 @@ open K3Helpers
 module U = K3Util
 
 let me_name = "me"
+let peers_name = "peers"
 
 let me_code = mk_global_val me_name t_addr
 let me_var = mk_var me_name
 
-let globals = [me_code]
+let peers_type = wrap_tset @: wrap_ttuple [t_addr; wrap_tmaybe t_string]
+let peers_empty = mk_global_val peers_name peers_type 
+let peers_code = peers_empty
+(*let update_peers = function *)
+     (*| [] -> peers := peers_empty*)
+     (*| ps -> peers := mk_global_val_init peers_name peers_type @: *)
+       (*List.fold_right*)
+         (*(fun (addr, mname) acc -> let n = match mname with*)
+           (*| None -> mk_const CNothing*)
+           (*| Some s -> mk_just @: mk_const @: CString s*)
+           (*in mk_combine *)
+             (*(mk_singleton peers_type @: mk_tuple [mk_const @: CAddress addr; n]) *)
+             (*acc*)
+         (*)*)
+         (*ps*)
+         (*(mk_empty peers_type)*)
+
+let globals = me_code::[peers_code]
 
 let add_globals ds = globals@ds
 let remove_globals ds = list_drop (List.length globals) ds
