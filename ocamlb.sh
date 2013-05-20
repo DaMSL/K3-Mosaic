f596a4ab86d3481f8555d8820810c1bef88898f8
Yotam Barnoy <yotambarnoy@gmail.com>
2013-05-20 13:18:38 -0400

    Modified _tags and ocamlb.sh

--- ocamlb.sh
+++ ocamlb.sh
@@ -1,1 +1,11 @@
-ocamlbuild -libs dynlink $@
+#!/bin/bash
+
+if [ ! -d "./bin" ]; then mkdir bin; fi
+ocamlbuild Driver.byte -build-dir ./bin $@
+if [ -f "./bin/src/Driver.byte" ]
+then CURDIR=`pwd` 
+     echo "#!/bin/bash" > ./bin/k3
+     echo "export BOLT_CONFIG=$CURDIR/bolt.cfg" >> ./bin/k3
+     echo "$CURDIR/bin/src/Driver.byte \$@" >> ./bin/k3
+     chmod +x ./bin/k3
+fi
