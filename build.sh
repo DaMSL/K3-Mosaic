11c914201ca67d20098ca428fbbbd709ba757718
Yotam Barnoy <yotambarnoy@gmail.com>
2013-05-23 15:11:38 -0400

    Modified build script to include debug info

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ ! -d "./bin" ]; then mkdir bin; fi
-ocamlbuild Driver.byte -build-dir ./bin $@
+ocamlbuild Driver.byte -build-dir ./bin -tag debug $@
 if [ -f "./bin/src/Driver.byte" ]
 then CURDIR=`pwd` 
      echo "#!/bin/bash" > ./bin/k3
      echo "export BOLT_CONFIG=$CURDIR/bolt.cfg" >> ./bin/k3
-     echo "$CURDIR/bin/src/Driver.byte \$@" >> ./bin/k3
+     echo "ocamlrun -b $CURDIR/bin/src/Driver.byte \$@" >> ./bin/k3
      chmod +x ./bin/k3
 fi
