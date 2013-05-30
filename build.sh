55b56d9750990c24ffc164c8e507e24c5f78a083
Yotam Barnoy <yotambarnoy@gmail.com>
2013-05-30 17:22:04 -0400

    Updated build script to be location independent
    Doesn't matter where you call it from

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#get directory of script
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+
+cd $SCRIPTPATH
 if [ ! -d "./bin" ]; then mkdir bin; fi
 ocamlbuild Driver.byte -build-dir ./bin -tag debug $@
 if [ -f "./bin/src/Driver.byte" ]
-then CURDIR=`pwd` 
-     echo "#!/bin/bash" > ./bin/k3
-     echo "export BOLT_CONFIG=$CURDIR/bolt.cfg" >> ./bin/k3
-     echo "ocamlrun -b $CURDIR/bin/src/Driver.byte \$@" >> ./bin/k3
+then echo "#!/bin/bash" > ./bin/k3
+     echo "export BOLT_CONFIG=$SCRIPTPATH/bolt.cfg" >> ./bin/k3
+     echo "ocamlrun -b $SCRIPTPATH/bin/src/Driver.byte \$@" >> ./bin/k3
      chmod +x ./bin/k3
 fi
+cd -
