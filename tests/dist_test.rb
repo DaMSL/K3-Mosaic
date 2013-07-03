fa60fc9e30aad0890c208c474004b172b03b2f3e
Yotam Barnoy <yotambarnoy@gmail.com>
2013-07-03 15:36:03 -0400

    Small updates to dist_test file

--- tests/dist_test.rb
+++ tests/dist_test.rb
@@ -81,13 +81,13 @@ def test_file(file, dbt_path, k3_path)
 	puts "cd #{dbt_path}"
 	Dir.chdir dbt_path
 
-	temp_file = File.join(curdir, "temp.trace")
+	trace_file = File.join(curdir, "temp.trace")
 	m3_file = File.join(curdir, "temp.m3")
 	err_file = File.join(curdir, "temp.err")
 
     # run dbtoaster to get interpreted updates
-    puts "./bin/dbtoaster -d LOG-INTERPRETER-UPDATES -d LOG-INTERPRETER-TRIGGERS -d LOG-M3 #{file} > #{temp_file}"
-    `./bin/dbtoaster -d PRINT-VERBOSE -d LOG-INTERPRETER-UPDATES -d LOG-INTERPRETER-TRIGGERS -d LOG-M3 #{file} > #{temp_file} 2> #{err_file}`
+    puts "./bin/dbtoaster -d LOG-INTERPRETER-UPDATES -d LOG-INTERPRETER-TRIGGERS -d LOG-M3 #{file} > #{trace_file}"
+    `./bin/dbtoaster -d PRINT-VERBOSE -d LOG-INTERPRETER-UPDATES -d LOG-INTERPRETER-TRIGGERS -d LOG-M3 #{file} > #{trace_file} 2> #{err_file}`
     check_error(curdir, err_file)
 
 	# run dbtoaster to get m3 file with distributed portion
@@ -104,40 +104,30 @@ def test_file(file, dbt_path, k3_path)
 	`#{k3_path} -p -i m3 -l k3 temp.m3 > temp.k3 2> #{err_file}`
 
     # create a k3 distributed file
-	puts "#{k3_path} -p -i m3 -l k3dist temp.m3 > temp2.k3dist"
-	`#{k3_path} -p -i m3 -l k3dist temp.m3 > temp2.k3dist 2> #{err_file}`
+	puts "#{k3_path} -p -i m3 -l k3disttest temp.m3 -trace #{trace_file} > temp.k3dist"
+	`#{k3_path} -p -i m3 -l k3disttest temp.m3 -trace #{trace_file} > temp.k3dist 2> #{err_file}`
 	check_error(curdir, err_file)
-    check_type_error(curdir, 'temp2.k3dist')
+    check_type_error(curdir, 'temp.k3dist')
 
-    # parse the distributed file
-	puts "#{k3_path} -p -i k3 temp2.k3dist > temp3.k3dist"
-	`#{k3_path} -p -i k3 -l k3 temp2.k3dist > temp3.k3dist 2> #{err_file}`
-	check_error(curdir, err_file)
-    check_type_error(curdir, 'temp3.k3dist')
+    ##parse the distributed file
+	#puts "#{k3_path} -p -i k3 temp2.k3dist > temp3.k3dist"
+	#`#{k3_path} -p -i k3 -l k3 temp2.k3dist > temp3.k3dist 2> #{err_file}`
+	#check_error(curdir, err_file)
+    #check_type_error(curdir, 'temp3.k3dist')
 
-    # compare the parsed file to the original file via AST
-	puts "#{k3_path} -p -i k3 -l k3ast temp2.k3dist > temp2.k3ast"
-	`#{k3_path} -p -i k3 -l k3ast temp2.k3dist > temp2.k3ast 2> #{err_file}`
-	check_error(curdir, err_file)
-    check_type_error(curdir, 'temp2.k3ast')
-	puts "#{k3_path} -p -i k3 -l k3ast temp3.k3dist > temp3.k3ast"
-	`#{k3_path} -p -i k3 -l k3ast temp3.k3dist > temp3.k3ast 2> #{err_file}`
-	check_error(curdir, err_file)
-    check_type_error(curdir, 'temp3.k3ast')
-    check_file_match(curdir, 'temp2.k3ast', 'temp3.k3ast')
-    File.unlink('temp2.k3ast')
-    File.unlink('temp3.k3ast')
-    File.unlink('temp3.k3dist')
-
-	# remove everything after "role client" from temp.k3
-	File.open("temp.k3dist", 'w') do |out|
-		out << File.open("temp2.k3dist").read.gsub(/role client.*/m, "")
-	end
-
-	# append the test from genmaps.rb
-  genmaps = File.join($cur_path, "./genmaps.rb")
-	puts "#{genmaps} --distrib temp.trace >> temp.k3dist"
-	`#{genmaps} --distrib temp.trace >> temp.k3dist`
+    ## compare the parsed file to the original file via AST
+	#puts "#{k3_path} -p -i k3 -l k3ast temp2.k3dist > temp2.k3ast"
+	#`#{k3_path} -p -i k3 -l k3ast temp2.k3dist > temp2.k3ast 2> #{err_file}`
+	#check_error(curdir, err_file)
+    #check_type_error(curdir, 'temp2.k3ast')
+	#puts "#{k3_path} -p -i k3 -l k3ast temp3.k3dist > temp3.k3ast"
+	#`#{k3_path} -p -i k3 -l k3ast temp3.k3dist > temp3.k3ast 2> #{err_file}`
+	#check_error(curdir, err_file)
+    #check_type_error(curdir, 'temp3.k3ast')
+    #check_file_match(curdir, 'temp2.k3ast', 'temp3.k3ast')
+    #File.unlink('temp2.k3ast')
+    #File.unlink('temp3.k3ast')
+    #File.unlink('temp3.k3dist')
 
     # create peer list
     peer_list = Array.new($num_peers - 1) do |i|
