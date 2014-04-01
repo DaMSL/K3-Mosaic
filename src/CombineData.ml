bef93530d93ef5c88f4d06b0f51d669f556f8a49
Yotam Barnoy <yotambarnoy@gmail.com>
2014-04-01 16:16:16 -0400

    combineData: add support for mutability qualifiers

--- src/CombineData.ml
+++ src/CombineData.ml
@@ -8,19 +8,23 @@ type params = {
   mutable data_files : string list;
   mutable order_file : string option;
   mutable map_file : string option;
+  mutable mut : bool; (* mutability qualifiers *)
 }
 
 let params = {
   data_files = [];
   order_file = None;
-  map_file = None
+  map_file = None;
+  mut = false;
 }
 
 let param_specs = Arg.align
   ["--order", Arg.String (fun s -> params.order_file <- Some s),
     "Order file to determine the read order of the logs";
    "--map", Arg.String (fun s -> params.map_file <- Some s),
-    "Map file to map file names to trigger names"
+    "Map file to map file names to trigger names";
+   "--mut", Arg.Unit (fun _ -> params.mut <- true),
+    "Use mutability qualifiers"
   ]
 
 let usage_msg = "Enter data files"
@@ -33,29 +37,33 @@ let parse_cmd_line () =
 let r_pipe = Str.regexp "|"
 let r_comma = Str.regexp ","
 
+let wrap_mut_rec s = if params.mut then Printf.sprintf "(%s,MemImmut)" s else s
+
 (* convert a tuple to a record *)
 let rec_of_tup s =
   let to_rec r =
-    let str =
-      String.concat "," @:
-        K3NewPrint.add_record_ids_str ~sep:"=" @: Str.split r s in
+    let l = K3NewPrint.add_record_ids @: Str.split r s in
+    let str = String.concat "," @:
+      List.map (fun (s, s') -> Printf.sprintf "%s=%s" s (wrap_mut_rec s')) l in
     Printf.sprintf "{%s}" str
   in
   if String.contains s '|' then to_rec r_pipe
   else if String.contains s ',' then to_rec r_comma
   else to_rec r_comma (* single value *)
 
+
 (* takes a list of trigger names and a list of lists of data lines *)
 (* returns a list of strings, each one being a line *)
 let combine_data t_d =
+  let mut_s = if params.mut then " MemImmut" else "" in
   let rec loop remain rem_num acc =
     if rem_num = 0 then acc else
     (* choose a random list to draw from *)
     let i = Random.int rem_num in
     let _, line, remain, rem_num, trig_id =
       List.fold_right (fun (t,l) (idx, out, remain, rem_num, trig_id) -> 
-        let do_just x  = (Printf.sprintf "Just %s" @: rec_of_tup x)::out in
-        let do_none () = "Nothing"::out in
+        let do_just x  = (Printf.sprintf "Just%s %s" mut_s @: rec_of_tup x)::out in
+        let do_none () = ("Nothing"^mut_s)::out in
         match l with
         | []           -> idx,   do_none (), (t,[])::remain, rem_num,   trig_id
         | [x]   when i=idx -> 
@@ -73,7 +81,9 @@ let combine_data t_d =
       | None   -> failwith "trigger not found" 
       | Some t -> (Printf.sprintf "\"%s\"" t)::line
     in
-    let line = K3NewPrint.add_record_ids_str ~prefix:"r" ~sep:"=" line in
+    (* convert every line to a record *)
+    let line = K3NewPrint.add_record_ids ~prefix:"r" line in
+    let line = List.map (fun (s,s') -> Printf.sprintf "%s=%s" s (wrap_mut_rec s')) line in
     let line = Printf.sprintf "{%s}" @: String.concat "," line in
     loop remain rem_num (line::acc)
   in
