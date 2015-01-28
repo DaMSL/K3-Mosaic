c825b49b5184e2c15cb050c7f399dc11e92eeb08
Yotam Barnoy <yotambarnoy@gmail.com>
2015-01-28 11:55:09 -0500

    travis: try new approach with shell script

---
+++ .travis-ci.sh
@@ -0,0 +1,19 @@
+
+echo "yes" | sudo add-apt-repository ppa:avsm
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time
+echo OCaml version
+ocaml -version
+pip install six
+
+export OPAMYES=1
+export OCAMLRUNPARAM=b
+
+opam init
+eval `opam config env`
+opam install ocamlfind
+
+./build_opt.sh
+./build_utils.sh
+tests/auto_test.py -l tests/passed_local_tests.txt
+tests/auto_test.py -d -l tests/passed_dist_tests.txt
