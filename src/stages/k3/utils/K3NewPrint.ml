23411383ba701d218b5629d77318ed914dac731d
Yotam Barnoy <yotambarnoy@gmail.com>
2015-02-15 19:17:57 -0500

    hack until k3new supports csv

--- src/stages/k3/utils/K3NewPrint.ml
+++ src/stages/k3/utils/K3NewPrint.ml
@@ -839,7 +839,9 @@ let channel_format c = function
   | JSON -> "json"
 
 let lazy_channel c chan_t chan_f = match chan_t with
-  | File s -> lps @@ Printf.sprintf "file \"%s\" %s" s (channel_format c chan_f)
+  (* hack for now *)
+  (* | File s -> lps @@ Printf.sprintf "file \"%s\" %s" s (channel_format c chan_f) *)
+  | File s -> lps @@ Printf.sprintf "file \"%s\" k3" s
   | Network(str, port) -> lps @@ "socket(\""^str^"\":"^string_of_int port^")"
 
 let rec lazy_resource_pattern c = function
