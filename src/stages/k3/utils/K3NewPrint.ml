5ce345a6012a4e1d5a17f384a81d53dcf1127d3a
Yotam Barnoy <yotambarnoy@gmail.com>
2015-04-07 13:13:36 -0400

    k3new: psv file support

--- src/stages/k3/utils/K3NewPrint.ml
+++ src/stages/k3/utils/K3NewPrint.ml
@@ -862,13 +862,11 @@ let lazy_trigger c id arg vars expr =
   lps "=" <| lsp () <| lazy_expr c @@ KH.mk_lambda arg expr <| lcut ())
 
 let channel_format c = function
-  | CSV  -> "csv"
+  | CSV  -> "psv"
   | JSON -> "json"
 
 let lazy_channel c chan_t chan_f = match chan_t with
-  (* hack for now *)
-  (* | File s -> lps @@ Printf.sprintf "file \"%s\" %s" s (channel_format c chan_f) *)
-  | File s -> lps @@ Printf.sprintf "file \"%s\" k3" s
+  | File s -> lps @@ Printf.sprintf "file \"%s\" %s" s (channel_format c chan_f)
   | Network(str, port) -> lps @@ "socket(\""^str^"\":"^string_of_int port^")"
 
 let rec lazy_resource_pattern c = function
