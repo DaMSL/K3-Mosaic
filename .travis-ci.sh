3e2c63acee01948ed13b1cc10c09cbde1c8c1303
Yotam Barnoy <yotambarnoy@gmail.com>
2015-06-25 15:22:25 -0400

    travis: added no-log option to be much quicker

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,5 +12,5 @@ opam install yojson
 
 ./build_opt.sh
 ./build_utils.sh
-tests/auto_test.py -l tests/passed_local_tests.txt
-tests/auto_test.py -d -l tests/passed_dist_tests.txt
+tests/auto_test.py -l tests/passed_local_tests.txt --no-log
+tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log
