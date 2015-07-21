6ed7a7f574c950fb470c7ff27f2dc5f353107d4a
Yotam Barnoy <yotambarnoy@gmail.com>
2015-07-21 17:46:02 -0400

    changed role binding to role2

--- src/stages/k3_dist/K3Dist.ml
+++ src/stages/k3_dist/K3Dist.ml
@@ -399,11 +399,11 @@ let job_timer  = create_ds "job_timer"  t_int ~init:(mk_cint job_timer_v)
 
 let job =
   let init =
-    mk_let ["role"] (mk_peek_or_error "bad role" @@ mk_var "role") @@
-    mk_if (mk_eq (mk_var "role") @@ mk_cstring "master") (mk_var job_master.id) @@
-    mk_if (mk_eq (mk_var "role") @@ mk_cstring "switch") (mk_var job_switch.id) @@
-    mk_if (mk_eq (mk_var "role") @@ mk_cstring "node")   (mk_var job_node.id)   @@
-    mk_if (mk_eq (mk_var "role") @@ mk_cstring "timer")  (mk_var job_timer.id)  @@
+    mk_let ["role2"] (mk_peek_or_error "bad role" @@ mk_var "role") @@
+    mk_if (mk_eq (mk_var "role2") @@ mk_cstring "master") (mk_var job_master.id) @@
+    mk_if (mk_eq (mk_var "role2") @@ mk_cstring "switch") (mk_var job_switch.id) @@
+    mk_if (mk_eq (mk_var "role2") @@ mk_cstring "node")   (mk_var job_node.id)   @@
+    mk_if (mk_eq (mk_var "role2") @@ mk_cstring "timer")  (mk_var job_timer.id)  @@
     mk_error "failed to find proper role"
   in
   create_ds "job" (mut t_int) ~init
