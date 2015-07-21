1eeb439bdaf4fb6243dfd836a0e4b899748d600e
Yanif Ahmad <yanif@jhu.edu>
2015-07-21 17:27:30 -0400

    Fix for checking role in new K3 engine

--- src/stages/k3_dist/K3Dist.ml
+++ src/stages/k3_dist/K3Dist.ml
@@ -394,15 +394,15 @@ let job_none_v   = 4
 
 let job_master = create_ds "job_master" t_int ~init:(mk_cint job_master_v)
 let job_switch = create_ds "job_switch" t_int ~init:(mk_cint job_switch_v)
-let job_node   = create_ds "job_node" t_int ~init:(mk_cint job_node_v)
-let job_timer  = create_ds "job_timer" t_int ~init:(mk_cint job_timer_v)
+let job_node   = create_ds "job_node"   t_int ~init:(mk_cint job_node_v)
+let job_timer  = create_ds "job_timer"  t_int ~init:(mk_cint job_timer_v)
 
 let job =
   let init =
-    mk_if (mk_eq (mk_var G.role.id) @@ mk_cstring "master") (mk_var job_master.id) @@
-    mk_if (mk_eq (mk_var G.role.id) @@ mk_cstring "switch") (mk_var job_switch.id) @@
-    mk_if (mk_eq (mk_var G.role.id) @@ mk_cstring "node")   (mk_var job_node.id)   @@
-    mk_if (mk_eq (mk_var G.role.id) @@ mk_cstring "timer")  (mk_var job_timer.id)  @@
+    mk_if (mk_neq (mk_slice' G.role.id [mk_cstring "master"]) (mk_empty @@ wrap_tset t_string)) (mk_var job_master.id) @@
+    mk_if (mk_neq (mk_slice' G.role.id [mk_cstring "switch"]) (mk_empty @@ wrap_tset t_string)) (mk_var job_switch.id) @@
+    mk_if (mk_neq (mk_slice' G.role.id [mk_cstring "node"])   (mk_empty @@ wrap_tset t_string)) (mk_var job_node.id)   @@
+    mk_if (mk_neq (mk_slice' G.role.id [mk_cstring "timer"])  (mk_empty @@ wrap_tset t_string)) (mk_var job_timer.id)  @@
     mk_error "failed to find proper role"
   in
   create_ds "job" (mut t_int) ~init
