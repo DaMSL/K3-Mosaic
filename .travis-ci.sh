3797ed423ae1e5a32a4e9496d2d2a9738ae367dc
Yotam Barnoy <yotambarnoy@gmail.com>
2015-07-24 14:05:19 -0400

    travis: add no-corrective mode to tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,5 +13,6 @@ opam install yojson
 ./build_utils.sh
 tests/auto_test.py -l tests/passed_local_tests.txt --no-log
 tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log
+tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log --no-correctives
 tests/auto_test.py -d -l tests/tpch_tests.txt --no-interp
 
