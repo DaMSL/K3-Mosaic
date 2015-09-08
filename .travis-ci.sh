210bda8e326ba56fc3cfc75ee32f679d580854b0
Yotam Barnoy <yotambarnoy@gmail.com>
2015-09-08 19:43:38 -0400

    travis: add more routing to tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,7 +12,7 @@ opam install yojson
 ./build_opt.sh
 ./build_utils.sh
 tests/auto_test.py -l tests/passed_local_tests.txt --no-log
-tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log
-tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log --no-correctives
+tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log -n 4
+tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log --no-correctives -n 4
 tests/auto_test.py -d -l tests/tpch_tests.txt --no-interp
 
