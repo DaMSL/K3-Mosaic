90671eaf7216f7266592a94b042ecf2681c60cb9
Yotam Barnoy <yotambarnoy@gmail.com>
2015-10-21 13:16:37 -0400

    newprint: add include for flatpolybuffer

--- src/stages/k3/utils/K3NewPrint.ml
+++ src/stages/k3/utils/K3NewPrint.ml
@@ -1501,8 +1501,8 @@ include \"Annotation/Set.k3\"
 include \"Annotation/Seq.k3\"
 include \"Annotation/Sets/SortedSet.k3\"
 include \"Annotation/Vector.k3\"
+include \"Annotation/FlatPolyBuffer.k3\"
 include \"Core/MosaicBuiltins.k3\"
-
 
 @:CArgs 2
 declare NATIONLoaderRP : collection {path: string} @Collection -> collection {ra:int, rb:string, rc:int, rd:string} @Collection -> collection {ra:int, rb:string, rc:int, rd:string} @Collection
