61a763d6fb3bd8d5cc9fb77b3ab508259a088ce2
Yotam Barnoy <yotambarnoy@gmail.com>
2015-11-11 00:46:40 -0500

    travis: update to no-opt-route mode

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,10 +15,10 @@ echo Running local tests...
 tests/auto_test.py -l tests/passed_local_tests.txt --no-log
 echo Done with local tests
 echo Running distributed tests...
-tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log -n 4
+tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log -n 4 --no-opt-route
 echo Done with distributed tests
 echo Running no-corrective tests...
-tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log --no-correctives -n 4
+tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log --no-correctives -n 4 --no-opt-route
 echo Done with no-corrective tests
 echo 'Creating TPCH files (no interpretation)'
 tests/auto_test.py -d -l tests/tpch_tests.txt --no-interp
