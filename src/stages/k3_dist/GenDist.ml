66c61b627582ee439099945a705d2d73612f13b7
Yotam Barnoy <yotambarnoy@gmail.com>
2015-11-10 22:32:27 -0500

    dist: make send_put more flexible

--- src/stages/k3_dist/GenDist.ml
+++ src/stages/k3_dist/GenDist.ml
@@ -355,53 +355,55 @@ let sw_send_fetch_fn c s_rhs_lhs s_rhs t =
       mk_set_all send_put_bitmap.id [mk_cfalse]
       ] @
 
-      List.map
-        (fun (stmt_id, (rhs_map_id, lhs_map_id)) ->
-          (* shuffle allows us to recreate the path the data will take from rhs to lhs *)
-          let shuffle_fn = K3Shuffle.find_shuffle_nm c stmt_id rhs_map_id lhs_map_id in
-          let shuffle_key, shuffle_empty_pat =
-            P.key_pat_from_bound c.p c.route_indices stmt_id lhs_map_id in
-          let shuffle_pat = P.get_shuffle_pat_idx c.p c.route_indices stmt_id lhs_map_id rhs_map_id in
-          (* route allows us to know how many nodes send data from rhs to lhs *)
-          let route_key, route_pat_idx = P.key_pat_from_bound c.p c.route_indices stmt_id rhs_map_id in
-          (* we need the types for creating empty rhs tuples *)
-          let rhs_map_types = P.map_types_with_v_for c.p rhs_map_id in
-          let tuple_types = wrap_t_calc' rhs_map_types in
-          mk_let ["sender_count"]
-            (* count up the number of IPs received from route *)
-            (R.route_lookup c rhs_map_id
-              (mk_cint rhs_map_id::route_key)
-              (mk_cint route_pat_idx) @@
-              mk_agg_bitmap'
-                ["acc", t_int]
-                (mk_add (mk_var "acc") @@ mk_cint 1)
-                (mk_cint 0)
-                R.route_bitmap.id) @@
-          mk_block [
-            (* fill in shuffle globals *)
-            mk_apply' shuffle_fn @@
-              shuffle_key @ [mk_cint shuffle_pat; mk_cint shuffle_empty_pat; mk_empty tuple_types];
-            (* loop over the shuffle bitmap *)
-            mk_iter_bitmap'
-              (mk_block [
-                (* mark the put bitmap *)
-                mk_insert_at send_put_bitmap.id (mk_var "ip") [mk_ctrue];
-                (* update the relevant stmt_cnt_list *)
-                mk_update_at_with send_put_ip_map_id (mk_var "ip") @@
-                  mk_lambda' send_put_ip_map_e @@
-                    mk_block [
-                      mk_insert_at "stmt_bitmap" (mk_cint stmt_id) [mk_ctrue];
-                      (* increment count *)
-                      mk_update_at_with stmt_cnt_list.id (mk_cint stmt_id) @@
-                        mk_lambda'' stmt_cnt_list.e @@
-                          mk_add (mk_var "count") @@ mk_var "sender_count";
-                      (* recreate tuple *)
-                      mk_tuple @@ ids_to_vars @@ fst_many @@ send_put_ip_map_e
-                    ]
-                ])
-              K3S.shuffle_bitmap.id
-          ])
-          s_rhs_lhs @
+      (List.flatten @@ List.map (fun s ->
+          let lmap = P.lhs_map_of_stmt c.p s in
+          let rmaps = P.rhs_maps_of_stmt c.p s in
+          List.map (fun rmap ->
+            (* shuffle allows us to recreate the path the data will take from rhs to lhs *)
+            let shuffle_fn = K3Shuffle.find_shuffle_nm c s rmap lmap in
+            let shuffle_key, shuffle_empty_pat =
+              P.key_pat_from_bound c.p c.route_indices s lmap in
+            let shuffle_pat = P.get_shuffle_pat_idx c.p c.route_indices s lmap rmap in
+            (* route allows us to know how many nodes send data from rhs to lhs *)
+            let route_key, route_pat_idx = P.key_pat_from_bound c.p c.route_indices s rmap in
+            (* we need the types for creating empty rhs tuples *)
+            let rhs_map_types = P.map_types_with_v_for c.p rmap in
+            let tuple_types = wrap_t_calc' rhs_map_types in
+            mk_let ["sender_count"]
+              (* count up the number of IPs received from route *)
+              (R.route_lookup c rmap
+                (mk_cint rmap::route_key)
+                (mk_cint route_pat_idx) @@
+                mk_agg_bitmap'
+                  ["acc", t_int]
+                  (mk_add (mk_var "acc") @@ mk_cint 1)
+                  (mk_cint 0)
+                  R.route_bitmap.id) @@
+            mk_block [
+              (* fill in shuffle globals *)
+              mk_apply' shuffle_fn @@
+                shuffle_key @ [mk_cint shuffle_pat; mk_cint shuffle_empty_pat; mk_empty tuple_types];
+              (* loop over the shuffle bitmap *)
+              mk_iter_bitmap'
+                (mk_block [
+                  (* mark the put bitmap *)
+                  mk_insert_at send_put_bitmap.id (mk_var "ip") [mk_ctrue];
+                  (* update the relevant stmt_cnt_list *)
+                  mk_update_at_with send_put_ip_map_id (mk_var "ip") @@
+                    mk_lambda' send_put_ip_map_e @@
+                      mk_block [
+                        mk_insert_at "stmt_bitmap" (mk_cint s) [mk_ctrue];
+                        (* increment count *)
+                        mk_update_at_with stmt_cnt_list.id (mk_cint s) @@
+                          mk_lambda'' stmt_cnt_list.e @@
+                            mk_add (mk_var "count") @@ mk_var "sender_count";
+                        (* recreate tuple *)
+                        mk_tuple @@ ids_to_vars @@ fst_many @@ send_put_ip_map_e
+                      ]
+                  ])
+                K3S.shuffle_bitmap.id
+            ]) rmaps) @@
+            P.stmts_of_t c.p t) @
       (* iterate over the result bitmap and buffer.
          Also count the number of sent msgs for this vid *)
       [
