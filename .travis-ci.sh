b47746ed0836c37cf03183442918f68f450a6622
Yotam Barnoy <yotambarnoy@gmail.com>
2016-01-18 21:45:11 -0500

    travis: fix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,10 +15,10 @@ echo Running local tests...
 tests/auto_test.py -l tests/passed_local_tests.txt --no-log
 echo Done with local tests
 echo Running distributed/corrective/no-isobatch tests...
-tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log -n 4 --no-opt-route --no-isobatch
+tests/auto_test.py -d -l tests/passed_dist_tests.txt --no-log -s 2 -n 4 --no-opt-route --no-run-isobatch --run-correctives
 echo Done with distributed/corrective/no-isobatch tests
 echo Running distributed/no-corrective/isobatch tests...
-tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log --no-correctives -n 4 --no-opt-route
+tests/auto_test.py -d -l tests/passed_dist_tests_no_corr.txt --no-log -s 2 -n 4 --no-opt-route
 echo Done with distributed/no-corrective/isobatch tests
 echo 'Creating TPCH files (no interpretation)'
 tests/auto_test.py -d -l tests/tpch_tests.txt --no-interp --no-opt-route
